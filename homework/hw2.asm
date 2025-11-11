94d7a82750b02e2b776949be5173b1d851f8041d
RetreyD <ktoto25012@gmail.com>
2025-11-11 19:03:15 +0200

    Update hw2.asm

--- homework/hw2.asm
+++ homework/hw2.asm
@@ -9,46 +9,53 @@ global _start
 
 int2str:
     mov edi, esi
-    cmp eax,0
-    jne .num
-    mov byte [esi],'0'
+    cmp eax, 0
+    jne .convert
+    mov byte [esi], '0'
     inc esi
-    jmp .done
-.num:
-    add esi,19
-    mov byte [esi],0
+    mov byte [esi], 0
+    mov eax, edi
+    ret
+.convert:
+    add esi, 19
+    mov byte [esi], 0
 .revloop:
-    xor edx,edx
-    mov ebx,10
+    xor edx, edx
+    mov ebx, 10
     div ebx
     dec esi
-    add dl,'0'
-    mov [esi],dl
-    test eax,eax
+    add dl, '0'
+    mov [esi], dl
+    test eax, eax
     jnz .revloop
-.done:
-    mov eax,esi
-    mov edx,edi
-    add edx,20
-    sub edx,eax
+    mov eax, esi
     ret
 
 _start:
-    mov eax,1234567
-    mov esi,buffer
+    mov eax, 1234567
+    mov esi, buffer
     call int2str
 
-    mov ecx,eax
-    mov ebx,1
-    mov eax,4
-    int 0x80
-    mov eax,4
-    mov ebx,1
-    mov ecx,newline
-    mov edx,1
+    mov edi, eax
+.nextchar:
+    cmp byte [edi], 0
+    je .strlen_done
+    inc edi
+    jmp .nextchar
+.strlen_done:
+    sub edi, eax
+    mov edx, edi
+    mov ecx, eax
+    mov ebx, 1
+    mov eax, 4
     int 0x80
 
-    mov eax,1
-    xor ebx,ebx
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, newline
+    mov edx, 1
     int 0x80
 
+    mov eax, 1
+    xor ebx, ebx
+    int 0x80
