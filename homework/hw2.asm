3e7a37b9069ed47a7d6197bbbdf8b9247390e11a
RetreyD <ktoto25012@gmail.com>
2025-11-11 18:44:53 +0200

    Update hw2.asm

--- homework/hw2.asm
+++ homework/hw2.asm
@@ -1,1 +1,48 @@
+section .bss
+buffer resb 20
+section .data
+newline db 0Ah, 0
+section .text
+global _start
+
+int2str:
+    mov edi, esi
+    cmp eax, 0
+    jne .num
+    mov byte [esi], '0'
+    inc esi
+    jmp .done
+.num:
+    add esi, 19
+    mov byte [esi], 0
+.revloop:
+    xor edx, edx
+    mov ebx, 10
+    div ebx
+    dec esi
+    add dl, '0'
+    mov [esi], dl
+    test eax, eax
+    jnz .revloop
+.done:
+    mov eax, esi
+    ret
+
+_start:
+    mov eax, 1234567
+    mov esi, buffer
+    call int2str
+    mov edx, 20
+    mov ecx, eax
+    mov ebx, 1
+    mov eax, 4
+    int 0x80
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, newline
+    mov edx, 1
+    int 0x80
+    mov eax, 1
+    xor ebx, ebx
+    int 0x80
 
