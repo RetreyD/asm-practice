fbe377c7308236e77e0c5c2f4047c370b4b66107
RetreyD <ktoto25012@gmail.com>
2025-11-11 19:02:06 +0200

    Update hw2.asm

--- homework/hw2.asm
+++ homework/hw2.asm
@@ -1,48 +1,54 @@
 section .bss
 buffer resb 20
+
 section .data
-newline db 0Ah, 0
+newline db 0Ah
+
 section .text
 global _start
 
 int2str:
     mov edi, esi
-    cmp eax, 0
+    cmp eax,0
     jne .num
-    mov byte [esi], '0'
+    mov byte [esi],'0'
     inc esi
     jmp .done
 .num:
-    add esi, 19
-    mov byte [esi], 0
+    add esi,19
+    mov byte [esi],0
 .revloop:
-    xor edx, edx
-    mov ebx, 10
+    xor edx,edx
+    mov ebx,10
     div ebx
     dec esi
-    add dl, '0'
-    mov [esi], dl
-    test eax, eax
+    add dl,'0'
+    mov [esi],dl
+    test eax,eax
     jnz .revloop
 .done:
-    mov eax, esi
+    mov eax,esi
+    mov edx,edi
+    add edx,20
+    sub edx,eax
     ret
 
 _start:
-    mov eax, 1234567
-    mov esi, buffer
+    mov eax,1234567
+    mov esi,buffer
     call int2str
-    mov edx, 20
-    mov ecx, eax
-    mov ebx, 1
-    mov eax, 4
+
+    mov ecx,eax
+    mov ebx,1
+    mov eax,4
     int 0x80
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, newline
-    mov edx, 1
+    mov eax,4
+    mov ebx,1
+    mov ecx,newline
+    mov edx,1
     int 0x80
-    mov eax, 1
-    xor ebx, ebx
+
+    mov eax,1
+    xor ebx,ebx
     int 0x80
 
